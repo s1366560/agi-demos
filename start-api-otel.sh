ae10e4dfbde7f0860b239839ca89bacf1a302f3c
s1366560 <s13665600@qq.com>
2026-02-09 16:58:54 +0800

    chore: remove unused start-api-otel.sh

--- start-api-otel.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export OTEL_EXPORTER_OTLP_ENDPOINT=http://localhost:4318
-export ENABLE_TELEMETRY=true
-export SERVICE_NAME=memstack
-export ENVIRONMENT=development
-
-cd /Users/tiejun.sun/Documents/github/vip-memory
-uv run uvicorn src.infrastructure.adapters.primary.web.main:app --host 0.0.0.0 --port 8000
